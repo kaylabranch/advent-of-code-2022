e146bc11195c4dff77ed75daebbc1290cc5cd910
Kayla <73916726+kaylabranch@users.noreply.github.com>
2022-12-05 23:56:28 -0600

    removing unnecessary data loaders for day6

--- days/day6/exercise2.js
+++ days/day6/exercise2.js
@@ -1,24 +1,22 @@
 import { input } from "./input.js";
-import { TextListToArrayOfStrings } from "../../data-loaders.js";
 
 // SOLUTION:
 export function myFunc(input) {
   const numPacketChars = 14;
 
   // format input
-  const data = TextListToArrayOfStrings(input);
+  const data = Array.from(input);
 
   // return length of string before and including marker
   let lengthToMarker = 0;
 
-  const charArray = Array.from(...data);
-  charArray.every((value, index) => {
+  data.every((value, index) => {
     // starting at {numPacketChars} char (num - 2 because it's zero indexed and greater than)
     if (index > numPacketChars - 2) {
       // look back at previous {numPacketChars} of items, add all to temp set
       const group = new Set();
       for (let i = 0; i < numPacketChars; i++) {
-        group.add(charArray[index - i]);
+        group.add(data[index - i]);
       }
 
       // if set length is {numPacketChars} we have our marker - return index and false
